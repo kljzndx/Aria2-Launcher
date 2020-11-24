7d98c98ce4260d973951d2b39ab22f2ddd921249
李继龙 <kljzndx@outlook.com>
2020-11-24 22:21:34 +0800

    优化代码排版

--- Aria2Launcher/ViewModel/MainViewModel.cs
+++ Aria2Launcher/ViewModel/MainViewModel.cs
@@ -13,7 +13,6 @@ namespace Aria2Launcher.ViewModel
         public MainViewModel()
         {
             BrowseAria2DirCommand = new RelayCommand(() => Configuration.Aria2DirPath = BrowseFolder());
-
             StartAria2Command = new RelayCommand(Aria2.StartAria2, () => !Aria2.IsRunning);
             StopAria2Command = new RelayCommand(Aria2.StopAria2, () => Aria2.IsRunning);
             ShowConfigureCommand = new RelayCommand(() => new Aria2ConfigureWindow().Show());
@@ -25,7 +24,6 @@ namespace Aria2Launcher.ViewModel
         public ObservableCollection<string> OutputList => Aria2.OutputList;
         
         public RelayCommand BrowseAria2DirCommand { get; }
-
         public RelayCommand StartAria2Command { get; }
         public RelayCommand StopAria2Command { get; }
         public RelayCommand ShowConfigureCommand { get; }
