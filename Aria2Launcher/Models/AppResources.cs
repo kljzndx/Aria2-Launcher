fc7f402124d6eea90a18cd5952f5d9dcb899b60e
李继龙 <kljzndx@outlook.com>
2022-02-20 15:51:39 +0800

    Remove unused code

--- Aria2Launcher/Models/AppResources.cs
+++ Aria2Launcher/Models/AppResources.cs
@@ -1,23 +1,18 @@
 ﻿using Aria2Launcher.Resources;
 
-using CommunityToolkit.Mvvm.ComponentModel;
-
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Aria2Launcher.Models
 {
-    public class AppResources : ObservableObject
+    public class AppResources
     {
-        public static AppResources Current;
-
         public AppResources()
         {
-            Current = this;
             Strings = new StringResource();
         }
 
-        public StringResource Strings { get; set; }
+        public StringResource Strings { get; }
     }
 }
