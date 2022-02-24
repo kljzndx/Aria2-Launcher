ac16cc40a4ba26d189e7a76ec66a493d3f7fbc37
李继龙 <kljzndx@outlook.com>
2022-02-24 09:31:09 +0800

    将一些工具方法移动到另一文件中

--- Aria2Launcher/Services/ConfigurationService.cs
+++ Aria2Launcher/Services/ConfigurationService.cs
@@ -13,7 +13,7 @@ using Newtonsoft.Json;
 
 namespace Aria2Launcher.Services
 {
-    public class ConfigurationService : ObservableObject
+    public partial class ConfigurationService : ObservableObject
     {
         private const string ConfFileName = "Aria2Launcher.json";
         private const string LnkFileName = "Aria2Launcher.lnk";
@@ -47,68 +47,6 @@ namespace Aria2Launcher.Services
             get => _isAutoStart;
             set => SetSetting(ref _isAutoStart, value);
         }
-
-        #region 自动启动
-
-        /// <summary>
-        /// 为本程序创建一个快捷方式。
-        /// </summary>
-        /// <param name="lnkFilePath">快捷方式的完全限定路径。</param>
-        /// <param name="args">快捷方式启动程序时需要使用的参数。</param>
-        private void CreateShortcut(string lnkFilePath, string args = "")
-        {
-            var dllPath = Assembly.GetEntryAssembly()?.Location;
-
-            if (string.IsNullOrWhiteSpace(dllPath))
-                throw new Exception("无法获取程序的路径");
-
-            var pathWithoutExtension = dllPath.Remove(dllPath.LastIndexOf('.') + 1);
-            var exePath = pathWithoutExtension + "exe";
-
-            var shell = new IWshRuntimeLibrary.WshShell();
-            var shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(lnkFilePath);
-            shortcut.TargetPath = exePath;
-            shortcut.Arguments = args;
-            shortcut.WorkingDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-            shortcut.Save();
-        }
-
-        public void SwitchAutoStart(bool needAutoStart)
-        {
-            var lnkPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), LnkFileName);
-            if (File.Exists(lnkPath))
-                File.Delete(lnkPath);
-
-            if (!needAutoStart)
-                return;
-
-            CreateShortcut(lnkPath, "--quiet");
-        }
-
-        #endregion
-        #region Aria2
-
-        public void BrowseAria2Folder()
-        {
-            using (var picker = new FolderBrowserDialog())
-            {
-                picker.RootFolder = Environment.SpecialFolder.MyComputer;
-                var dialogResult = picker.ShowDialog();
-                Aria2DirPath = dialogResult == DialogResult.OK ? picker.SelectedPath : Aria2DirPath;
-            }
-        }
-
-        public void ViewAria2OfficialSite()
-        {
-            using (var process = new Process())
-            {
-                process.StartInfo.FileName = "https://github.com/aria2/aria2/releases/latest";
-                process.StartInfo.UseShellExecute = true;
-                process.Start();
-            }
-        }
-
-        #endregion
 
         private void SetSetting<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
         {
